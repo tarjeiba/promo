29323a08ea24ca1dec32c5f106d54da4073a5fb2
Tarjei Bærland <tarjeibaerland@gmail.com>
2018-07-24 23:36:01 +0200

    Opprinnelig commeit
    Endret til ett repo med org- og html-filer i samme.

---
+++ js/popup.js
@@ -0,0 +1,6 @@
+// Det følgende er hentet fra https://www.w3schools.com/howto/howto_js_popup.asp
+
+function myPopupFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
